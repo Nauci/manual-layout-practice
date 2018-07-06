d01833c92124989e6c456379129f462dd7074836
Nauci <eleinesun@gmail.com>
2018-07-06 03:33:52 -0700

    View controllers updated with subview assets.

--- ManualLayoutPractice/AViewController.swift
+++ ManualLayoutPractice/AViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class AViewController: UIViewController {
-
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nil, bundle: nil)
         self.title = "A"
@@ -19,9 +19,28 @@ class AViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    let a = UIView()
+    let b = UIView()
+    let c = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        a.backgroundColor = UIColor.red
+        b.backgroundColor = UIColor.green
+        c.backgroundColor = UIColor.blue
+        
+        b.addSubview(c)
+        a.addSubview(b)
+        self.view.addSubview(a)
+        
         self.view.backgroundColor = UIColor.white
+
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        a.frame = CGRect(x:0, y:0, width: a.superview!.bounds.width, height: a.superview!.bounds.width)
+        b.frame = CGRect(x:50, y:50, width: b.superview!.bounds.width-100, height: b.superview!.bounds.height-100)
+        c.frame = CGRect(x:50, y:50, width: c.superview!.bounds.width-100, height: c.superview!.bounds.height-100)
+    }
 }
--- ManualLayoutPractice/BViewController.swift
+++ ManualLayoutPractice/BViewController.swift
@@ -18,10 +18,42 @@ class BViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    let a = UIView()
+    let b = UIView()
+    let c = UIView()
+    let d = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
+        
+        a.addSubview(d)
+        a.addSubview(c)
+        a.addSubview(b)
+        self.view.addSubview(a)
+        
+        a.backgroundColor = UIColor(red: 35/255, green: 207/255, blue: 231/255, alpha: 1)
+        b.backgroundColor = UIColor.white
+        c.backgroundColor = UIColor.white
+        d.backgroundColor = UIColor.white
+        self.view.backgroundColor = UIColor.white
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let w = self.view.bounds.width
+        let h = self.view.bounds.height
+        
+        let margin = w * 0.03
+        let padding = w * 0.035
+    
+        a.frame = CGRect(x: margin, y: margin, width: w * 0.94, height: h * 0.3)
+        b.frame = CGRect(x: padding, y: padding, width: w * 0.19, height: a.bounds.height - padding * 2)
+        c.frame = CGRect(x: padding * 2 + b.bounds.width, y: padding, width: a.bounds.width - padding * 3 - b.bounds.width, height: h * 0.09)
+        d.frame = CGRect(x: padding * 2 + b.bounds.width, y: c.bounds.height + padding * 2, width: c.bounds.width, height: a.bounds.height - padding * 3 - c.bounds.height)
+        
     }
 
+    
 }
